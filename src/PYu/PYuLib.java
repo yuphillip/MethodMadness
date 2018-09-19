df8ce62bb29603e66fe8037fbb709b9a7d44ed44
Phillip <pyu6253@bths.edu>
2018-09-19 17:22:34 -0400

    AtHomeWork

--- src/PYu/PYuLib.java
+++ src/PYu/PYuLib.java
@@ -1,5 +1,8 @@
 package PYu;
 
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
 public class PYuLib {
 
     public static String cutOut(String mainStr, String subStr)
@@ -19,5 +22,31 @@ public class PYuLib {
         String newDate = day + "-" + month + "-" + year;
         return newDate;
     }
+    public static Integer sumUpTo(Integer num)
+    {
+        Integer sum = 0;
+        for(Integer stat = num; stat >= 0; stat-=1)
+        {
+            sum += stat;
+        }
+        return sum;
+    }
+    public static Boolean isFibonnaci(Integer num)
+    {
+        Integer end = 0;
+        Integer start = 0;
+        Integer add = 1;
+        while(end < num)
+        {
+            end = start + add;
+            start = add;
+            add = end;
+        }
+        if (end == num) {
+            return TRUE;
+        }
+        return FALSE;
+    }
+    
 
 }
--- src/PYu/TestFile.java
+++ src/PYu/TestFile.java
@@ -1,15 +1,15 @@
 package PYu;
 
 
-import static PYu.PYuLib.cutOut;
-import static PYu.PYuLib.dateStr;
+import static PYu.PYuLib.*;
 
 public class TestFile {
 
     public static void main(String[] args)
     {
         System.out.println(cutOut("hotness","ne"));
-        System.out.println(dateStr("11/24/01"));
-        System.out.Println(fibonacci);
+        System.out.println(dateStr("11/24/2001"));
+        System.out.println(sumUpTo(20));
+        System.out.println(isFibonnaci(89));
      }
 }
